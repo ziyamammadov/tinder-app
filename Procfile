4d779f034221d8be24489e0d38363b7f99f0cb75
mmmdovziya900 <mmmdovziya900@gmail.com>
2019-12-12 10:26:26 -0800

    --remote db

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp ./target/classes:target/deps/* TinderApp
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* TinderApp
--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -10,9 +10,9 @@ public class DBConnection {
     public static Connection get() throws SQLException {
         if (conn == null) {
             conn = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/tinder",
-                    "postgres",
-                    "123"
+                    "jdbc:postgresql://ec2-174-129-255-72.compute-1.amazonaws.com/dd5jboni9dpg7h",
+                    "lktkgqkemluryv",
+                    "ff6190a0bae6a3718fc03ed7bd30d62dafaf01ece4e86d2e92d3c5aa0fa8360d"
             );
         }
         return conn;
